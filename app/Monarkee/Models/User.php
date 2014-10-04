56b3d0d0419ff58e4859a656f62a601498495dcf
David Hemphill <davidlee.hemphill@gmail.com>
2014-10-04 00:52:24 -0500

    Adding sample project User model

---
+++ app/Monarkee/Models/User.php
@@ -0,0 +1,35 @@
+<?php namespace Monarkee\Models;
+
+use Monarkee\Bumble\Fields\PasswordField;
+use Monarkee\Bumble\Fields\TextField;
+use Monarkee\Bumble\Models\BumbleModel;
+use Monarkee\Bumble\Models\User as BumbleUser;
+
+class User extends BumbleUser
+{
+
+    /**
+     * @var array
+     */
+    public $validation = [
+        'first_name' => 'required',
+        'last_name' => 'required',
+        'password' => 'required',
+    ];
+
+    /**
+     *
+     */
+    public function setComponents()
+    {
+        $this->components = [
+            new TextField('username'),
+            new TextField('email'),
+            new PasswordField('password'),
+            new TextField('prefix'),
+            new TextField('first_name'),
+            new TextField('middle_name'),
+            new TextField('last_name'),
+        ];
+    }
+}
