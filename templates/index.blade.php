793da86faacb76d0cdf2f9dcd136e2ac03e5ac75
David Hemphill <davidlee.hemphill@gmail.com>
2014-10-18 01:43:03 -0500

    Remove unused view

--- templates/index.blade.php
+++
@@ -1,8 +0,0 @@
-@foreach (Post::section('blog')->limit(5)->get() as $entry)
-    {{ $entry->title }}
-    {{ $entry->content }}
-
-    @foreach ($entry->categories as $category )
-        {{{ $category->title }}} / {{{ $category->slug }}}
-    @endforeach
-@endforeach
