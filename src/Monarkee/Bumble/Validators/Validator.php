59dd9b0935a67db7f2134e9d3756c9f887bdd62c
David Hemphill <davidlee.hemphill@gmail.com>
2014-10-30 00:54:47 -0500

    Update Validator

--- src/Monarkee/Bumble/Validators/Validator.php
+++ src/Monarkee/Bumble/Validators/Validator.php
@@ -8,36 +8,95 @@ abstract class Validator
     /**
      * @var LaravelValidator
      */
-    private $validator;
+    protected $validator;
+
+    /**
+     * @var array
+     */
+    protected $rules;
+
+    /**
+     * @var
+     */
+    protected $updateRules;
 
     public function __construct(LaravelValidator $validator)
     {
         $this->validator = $validator;
     }
 
-  /**
-   * Do the validation
-   * @param  array $input The input rules
-   * @return bool
-   */
+    /**
+     * Do the validation
+     *
+     * @param  array $input The input rules
+     * @param array  $rules
+     * @throws ValidationException
+     * @return bool
+     */
   public function validate($input, $rules = [])
   {
-    $rules = $rules ?: $this->rules;
+    $rules = $rules ?: $this->getRules();
 
     $validator = $this->validator->make($input, $rules);
 
-    if ($validator->fails()) throw new ValidationException($validator->messages(), 1);
+    if ($validator->fails()) throw new ValidationException($validator->messages(), 'Validation failed.');
 
     return true;
   }
 
-  public function validateForCreate($input)
+    /**
+     * @return array
+     */
+    public function getRules()
+    {
+        return $this->rules;
+    }
+
+    /**
+     * @param array $rules
+     */
+    public function setRules($rules)
+    {
+        $this->rules = $rules;
+    }
+
+    /**
+     * Validate the model for creation
+     *
+     * @param $input
+     * @return bool
+     * @throws ValidationException
+     */
+    public function validateForCreate($input)
   {
-    return $this->validate($input, $this->rules);
+    return $this->validate($input, $this->getRules());
   }
 
-  public function validateForUpdate($input)
+    /**
+     *
+     *
+     * @param $input
+     * @return bool
+     * @throws ValidationException
+     */
+    public function validateForUpdate($input)
   {
-    return $this->validate($input, $this->updateRules);
+    return $this->validate($input, $this->getUpdateRules());
   }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdateRules()
+    {
+        return $this->updateRules;
+    }
+
+    /**
+     * @param mixed $updateRules
+     */
+    public function setUpdateRules($updateRules)
+    {
+        $this->updateRules = $updateRules;
+    }
 }
