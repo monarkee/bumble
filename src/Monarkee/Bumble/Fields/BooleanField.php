79de067b0cb1c7b2e6f41d49ec0596e6f4718189
David Hemphill <davidlee.hemphill@gmail.com>
2014-10-31 23:33:23 -0500

    Move BinaryField to BooleanField

--- src/Monarkee/Bumble/Fields/BooleanField.php
+++ src/Monarkee/Bumble/Fields/BooleanField.php
@@ -1,14 +1,17 @@
 <?php namespace Monarkee\Bumble\Fields;
 
 use Monarkee\Bumble\Fields\Field;
+use Monarkee\Bumble\Interfaces\FieldInterface;
 
-class BinaryField extends Field
+class BooleanField extends Field implements FieldInterface
 {
-    public function isBinaryField()
-    {
-        return true;
-    }
-
+    /**
+     * Process the model, assign its input and return the model to the user
+     *
+     * @param $model
+     * @param $input
+     * @return mixed
+     */
     public function process($model, $input)
     {
         $column = $this->getColumn();
