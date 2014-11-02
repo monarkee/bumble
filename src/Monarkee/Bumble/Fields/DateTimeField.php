df97f72590d41b4bfc97c03aa671811238026842
David Hemphill <davidlee.hemphill@gmail.com>
2014-11-02 00:57:32 -0500

    Fix bug in DateTimeField where the row had a column value like 0000-00-00 00:00:00

--- src/Monarkee/Bumble/Fields/DateTimeField.php
+++ src/Monarkee/Bumble/Fields/DateTimeField.php
@@ -21,4 +21,27 @@ class DateTimeField extends TextField implements FieldInterface
     {
         return Carbon::parse($value)->format($this->getFormat());
     }
+
+    /**
+     * Process the DateTimeField data
+     *
+     * @param $model
+     * @param $input
+     * @return BumbleModel
+     */
+    public function process($model, $input)
+    {
+        $column = $this->getColumn();
+
+        // Handle a special case where the data in the database is 0000-00-00 00:00:00
+        if ($input[$column] == '-0001-11-30 00:00:00')
+        {
+            $model->{$column} = Carbon::now();
+        }
+        elseif(isset($input[$column])) {
+            $model->{$column} = $input[$column];
+        }
+
+        return $model;
+    }
 }
