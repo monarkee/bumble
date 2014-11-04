acde510e5f3f7c85bce619aa083648992189944c
David Hemphill <davidlee.hemphill@gmail.com>
2014-11-04 13:41:08 -0600

    Namespace Bumble migration classes

--- src/migrations/2013_12_14_212553_create_password_reminders_table.php
+++ src/migrations/2013_12_14_212553_create_password_reminders_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordRemindersTable extends Migration {
+class BumbleCreatePasswordRemindersTable extends Migration {
 
 	/**
 	 * Run the migrations.
