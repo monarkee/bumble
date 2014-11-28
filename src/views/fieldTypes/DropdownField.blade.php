916446a1a9b9c4c075665c1f3326ec6887109ea5
David Hemphill <davidlee.hemphill@gmail.com>
2014-11-28 02:13:49 -0600

    Update appearance of DropDownField

--- src/views/fieldTypes/DropdownField.blade.php
+++ src/views/fieldTypes/DropdownField.blade.php
@@ -1,6 +1,9 @@
-<div class="form__text">
-    <?php $fieldName = $model->fieldIsRequired($field) ? $field->getTitle() . ' (required)' : $field->getTitle(); ?>
-    {{ Form::label($field->getColumn(), $fieldName, ['class' => 'form__text-label']) }}
-    <?php $values = $field->getValues(); ?>
-    {{ Form::select($field->getColumn(), $values, (!isset($editing) ? $field->getDefaultOption() : null), ['class' => 'form__text-input', 'placeholder' => $field->getPlaceholder()]) }}
+<div class="control g-row">
+    <div class="g-col-2 tar">
+        {{ Form::label($field->getColumn(), $field->getTitle(), ['class' => 'label label--tar'.$model->getRequiredClass($field)]) }}
+    </div>
+    <div class="g-col-10">
+        <?php $values = $field->getValues(); ?>
+        {{ Form::select($field->getColumn(), $values, (!isset($editing) ? $field->getDefaultOption() : null), ['class' => 'input input1', 'placeholder' => $field->getPlaceholder()]) }}
+    </div>
 </div>
