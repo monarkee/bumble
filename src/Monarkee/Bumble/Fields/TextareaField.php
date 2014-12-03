102da6275a23ee11ce7714b3114d755e2583a9f6
David Hemphill <davidlee.hemphill@gmail.com>
2014-12-02 23:38:38 -0600

    Enable setting height of textareas

--- src/Monarkee/Bumble/Fields/TextareaField.php
+++ src/Monarkee/Bumble/Fields/TextareaField.php
@@ -4,5 +4,13 @@ use Monarkee\Bumble\Fields\Field;
 use Monarkee\Bumble\Interfaces\FieldInterface;
 
 class TextareaField extends Field implements FieldInterface {
-
+    /**
+     * Get the number of rows a textarea should have
+     *
+     * @return bool
+     */
+    public function getRowsOption()
+    {
+        return isset($this->options['rows']) ? $this->options['rows'] : 10;
+    }
 }
