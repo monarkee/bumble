f5a786a962e0fcd2885e44a00addf32f2d52b408
David Hemphill <davidlee.hemphill@gmail.com>
2015-01-30 19:35:16 -0600

    Update service provider

--- Monarkee/Bumble/Providers/BumbleServiceProvider.php
+++ Monarkee/Bumble/Providers/BumbleServiceProvider.php
@@ -1,4 +1,4 @@
-<?php namespace Monarkee\Bumble;
+<?php namespace Monarkee\Bumble\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use League\Flysystem\Adapter\AwsS3 as S3Adapter;
@@ -21,10 +21,13 @@ class BumbleServiceProvider extends ServiceProvider {
     public function boot()
     {
         // Publish the config files and public assets
-        $this->publishes([
-            __DIR__.'/../../config/bumble.php' => config_path('bumble.php'),
-            __DIR__.'/../../../public/' => public_path().'/packages/monarkee/bumble',
-        ]);
+        $this->publishes([__DIR__.'/../../config/bumble.php' => config_path('bumble.php')], 'config');
+
+        $this->publishes([__DIR__.'/../../../public/' => public_path().'/packages/monarkee/bumble'], 'views');
+
+        $this->publishes([__DIR__.'/../../../database/migrations/' => base_path().'/database/migrations'], 'migrations');
+
+        $this->publishes([__DIR__.'/../../../database/seeds/' => base_path().'/database/seeds'], 'seeds');
 
         // Include custom Bumble configuration
         $this->includeCustomConfiguration();
@@ -37,26 +40,9 @@ class BumbleServiceProvider extends ServiceProvider {
      */
     public function register()
     {
-        $this->app->singleton('bumblestr', function ()
-        {
-            return $this->app->make('Monarkee\Bumble\Support\BumbleStr');
-        });
+        $this->registerBindings();
 
-        $this->app->singleton('bumble-gravatar', function ()
-        {
-            return $this->app->make('Monarkee\Bumble\Support\Gravatar');
-        });
-
-        $this->app->bind('Monarkee\Bumble\Repositories\ModelRepository', 'Monarkee\Bumble\Repositories\ArrayConfigModelRepository');
-
-        /*
-         * Create aliases for the dependency.
-         */
-        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-        $loader->alias('BumbleStr', 'Monarkee\Bumble\Support\Facades\BumbleStr');
-        $loader->alias('BumbleGravatar', 'Monarkee\Bumble\Support\Facades\Gravatar');
-
-        // config(['bumble', __DIR__.'/../../config/bumble.php']);
+        $this->createAliases();
 
         // Merge the config values so they don't have to have a complete configuration
         $this->mergeConfigFrom(__DIR__.'/../../config/bumble.php', 'bumble');
@@ -73,6 +59,36 @@ class BumbleServiceProvider extends ServiceProvider {
     public function provides()
     {
         return array('bumble.bumblestr', 'bumble.bumble-gravatar');
+    }
+
+    /**
+     * Register the IoC Bindings
+     * @return void
+     */
+    public function registerBindings()
+    {
+        $this->app->singleton('bumblestr', function ()
+        {
+            return $this->app->make('Monarkee\Bumble\Support\BumbleStr');
+        });
+
+        $this->app->singleton('bumble-gravatar', function ()
+        {
+            return $this->app->make('Monarkee\Bumble\Support\Gravatar');
+        });
+
+        $this->app->bind('Monarkee\Bumble\Repositories\ModelRepository', 'Monarkee\Bumble\Repositories\ArrayConfigModelRepository');
+    }
+
+    /**
+     * Create aliases for the dependency.
+     * @return void
+     */
+    public function createAliases()
+    {
+        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+        $loader->alias('BumbleStr', 'Monarkee\Bumble\Support\Facades\BumbleStr');
+        $loader->alias('BumbleGravatar', 'Monarkee\Bumble\Support\Facades\Gravatar');
     }
 
     /**
