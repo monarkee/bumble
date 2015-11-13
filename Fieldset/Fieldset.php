b541357640f9f9b50db00c39e491783e3794b32e
David Hemphill <davidlee.hemphill@gmail.com>
2015-11-13 02:46:06 -0600

    Comment some things

--- Fieldset/Fieldset.php
+++ Fieldset/Fieldset.php
@@ -6,12 +6,28 @@ use Exception;
 
 class Fieldset
 {
+    /**
+     * The fields for the Fieldset
+     * @var array
+     */
     public $fields;
 
+    /**
+     * The tabs for the Fieldset
+     * @var array
+     */
     protected $tabs;
 
+    /**
+     * The original configuration passed in
+     * @var array
+     */
     protected $originalFields;
 
+    /**
+     * Create a new instance
+     * @param array $fields
+     */
     public function __construct(array $fields = [])
     {
         $this->originalFields = $fields;
@@ -19,6 +35,10 @@ class Fieldset
         $this->assignTabs($this->originalFields);
     }
 
+    /**
+     * Get the fields for the fieldset
+     * @return array
+     */
     public function getFields()
     {
         $this->checkForFields();
@@ -27,6 +47,7 @@ class Fieldset
     }
 
     /**
+     * Assign the tabs for the Fieldset
      * @param array $tabs
      * @internal param $field
      */
@@ -49,6 +70,11 @@ class Fieldset
         }
     }
 
+    /**
+     * Get a tab from the Fieldset
+     * @param  string $tabId
+     * @return array
+     */
     public function getTabFields($tabId)
     {
         $this->checkForFields();
@@ -56,16 +82,29 @@ class Fieldset
         return $this->tabs[$tabId];
     }
 
+    /**
+     * Get a field from the Fieldset
+     * @param  string $fieldName
+     * @return array
+     */
     public function getField($fieldName)
     {
         return $this->fields[$fieldName];
     }
 
+    /**
+     * Get the tabs for the Fieldset
+     * @return array
+     */
     public function getTabs()
     {
         return $this->tabs;
     }
 
+    /**
+     * Check if the Fieldset has fields assigned
+     * @return void
+     */
     protected function checkForFields()
     {
         if (empty($this->fields)) {
